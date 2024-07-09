a03d8791f21dabe9ad6e4709c60822ee12fb3de1
Артем Бакаев <vibrus93@live.ru>
2024-07-09 09:51:01 +0300

    Cleaning code Changed zip method to x64, removed archive size limit Added console log and progress

--- copy_app/Program.cs
+++ copy_app/Program.cs
@@ -11,28 +11,73 @@ var dir = new DirectoryInfo(folder).GetFiles("*", SearchOption.AllDirectories);
 List<string> filesToAdd = [];
 HashHistory? fileHistory;
 
-foreach (var file in dir)
+
+for (int i = 0; i < dir.Length; i++)
 {
-    if(comaprer.HasHistory)
+    if (comaprer.HasHistory)
     {
-        fileHistory = comaprer.History?.HashHistories.FirstOrDefault(x => x.FilePath == file.FullName);
-                
-        if(fileHistory != null && !await comaprer.IsSimillarByHash(fileHistory, file.FullName))
+        fileHistory = comaprer.History?.HashHistories.FirstOrDefault(x => x.FilePath == dir[i].FullName);
+
+        if (fileHistory != null && !await comaprer.IsSimillarByHash(fileHistory, dir[i].FullName))
         {
-            filesToAdd.Add(file.FullName);
+            filesToAdd.Add(dir[i].FullName);
         }
     }
     else
     {
-        filesToAdd.Add(file.FullName);
+        filesToAdd.Add(dir[i].FullName);
     }
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"Processing files... {Math.Round((decimal)i/dir.Length * 100)}%");
 }
 
+Console.WriteLine($"History parsing completed.");
+Console.WriteLine();
+
+Console.WriteLine("Saving history.");
 await comaprer.SaveHashHistory(dir.Select(x => x.FullName));
 
+
 if(filesToAdd.Count != 0)
-    using (ZipFile zip = new())
+{
+    var pBar = 0;
+    var pMax = filesToAdd.Count;
+
+    Console.WriteLine("Creating zip file...");
+
+    using ZipFile zip = new()
     {
-        zip.AddFiles(filesToAdd);
-        zip.Save(Path.Combine(folder, "arch.zip"));
-    }
+        UseZip64WhenSaving = Zip64Option.Always,
+        CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression,
+        CompressionMethod = CompressionMethod.BZip2 // or CompressionMethod.Deflate or CompressionMethod.None
+    };
+
+    zip.AddFiles(filesToAdd);
+
+    Console.WriteLine();
+    Console.WriteLine("Entry bytes reading.");
+    var (Left, Top) = Console.GetCursorPosition();
+
+    zip.SaveProgress += (sender, e) =>
+    {
+        if (e.EventType == ZipProgressEventType.Saving_BeforeWriteEntry)
+        {
+            Console.SetCursorPosition(Left, Top);
+            Console.WriteLine($"Writing zip... {Math.Round((decimal)pBar++ / pMax * 100)}%");
+        }
+    };
+
+    (Left, Top) = Console.GetCursorPosition();
+
+    Console.SetCursorPosition(Left, Top+3);
+
+    zip.Save(@"arch.zip");
+    Console.WriteLine("Archive saved.");
+}
+else
+{
+    Console.WriteLine("Files has no changes.");
+}
+
+Console.ReadKey();
